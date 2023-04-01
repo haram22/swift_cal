e7e89a36c61d4b69e7f4b141447dbbbf6f84a8a8
haram22 <97924765+haram22@users.noreply.github.com>
2023-04-01 20:30:12 +0900

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by 김하람 on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
